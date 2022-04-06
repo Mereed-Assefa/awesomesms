044e8570da4cbd922cb05a9e9ff8494292c52d26
Mereed-Assefa <57495522+Mereed-Assefa@users.noreply.github.com>
2022-04-06 16:05:12 +0300

    b b

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./styles.css";
-import Counter from "./components/Counter";
-import Display from "./components/Display";
-import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <Display mycount={count} />
-      <Counter increment={setCount} />
+      <h1>Welcome</h1>
     </div>
   );
 }
+//
+
+// export default function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div className="App">
+//       <Display mycount={count} />
+//       <Counter increment={setCount} />
+//     </div>
+//   );
+// }
